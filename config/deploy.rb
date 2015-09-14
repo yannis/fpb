b64deccb6f4f37a91d71a6934ee19b8af9f5d765
yannis <yannisjaquet@me.com>
2015-09-14 12:03:21 +0200

    ignore database.yml file in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, 'git@github.com:yannis/fpb.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('config/application.yml')
+set :linked_files, fetch(:linked_files, []).push('config/application.yml', 'config/database.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
